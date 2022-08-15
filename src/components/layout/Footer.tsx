aac342cc186aad6b97329e63174b5cb4d3fe20f3
yangjianfei <yangjianfei@reconova.cn>
2022-08-15 12:26:58 +0800

    fix:添加访问量

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -13,6 +13,8 @@ const Footer: FC = () => {
     return (
         <>
             <div className='border-t p16 tx-center'>
+                <span>翻牌次数:</span>
+                <img className="mx12" src="https://profile-counter.glitch.me/heifahaizei/count.svg" alt="翻牌次数" />
                 2020-{new Date().getFullYear()}
                 <a className='mx12' href='https://github.com/YangJianFei' target='_blank' rel='noopener noreferrer'>
                     <Icon name='github' style={{ verticalAlign: 'middle' }} />
---
+++ src/utils/test.ts
@@ -0,0 +1,12 @@
+interface Person {
+    name: string;
+    age: string;
+}
+
+class Man implements Person {
+    name = '';
+    age = '';
+    go = () => { }
+}
+
+export default {}
